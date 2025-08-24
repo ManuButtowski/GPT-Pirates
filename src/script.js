703a56fc2794b97db9493e9cc0e577353fb3f724
ManuelSolarBueno <manuelsolarbueno@gmail.com>
2025-08-24 16:33:37 +0200

    first commit Inicio base de proyecto

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const startBtn = document.getElementById('startBtn');
+
+startBtn.addEventListener('click', () => {
+  // Aquí cambias el destino al archivo de tu primer nivel
+  window.location.href = "level1.html"; 
+});
